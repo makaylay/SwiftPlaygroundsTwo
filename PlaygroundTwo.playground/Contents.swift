5c03c8671ab901e9f8d2a05692b29297f573a45c
Makayla <mcsyanos@gmail.com>
2018-01-30 10:55:00 -1000

    Completed SW2

--- PlaygroundTwo.playground/Contents.swift
+++ PlaygroundTwo.playground/Contents.swift
@@ -18,11 +18,24 @@ if goodGrade == "I'm not doing very well"
     print("I'm doing well in this class")
 }
 
-//Part 4
+//Part 4 if and else statment
 if goodGrade == "I've been doing all my work"
 {
-    print("I'm doing well in this class")
+    print("I should be getting an A at this point")
 }
-else{
-    print("I'm not doing too well in this class")
+else
+{
+    print("I need to work harder in this class")
 }
+
+//Part 4 Using Boolean variables in if Statements
+var gradeA = true
+if gradeA {
+    print("I really like this class")
+}
+else
+{
+    print("I'm not so sure about coding")
+}
+
+
